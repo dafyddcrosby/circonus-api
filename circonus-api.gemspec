68d579f727986eb1cfa16027e060ee5dac6c4434
Ben Marini <bmarini@gmail.com>
2012-12-04 16:02:35 -0800

    Initial commit

---
+++ circonus-api.gemspec
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'circonus-api/version'
+
+Gem::Specification.new do |gem|
+  gem.name          = "circonus-api"
+  gem.version       = Circonus::Api::VERSION
+  gem.authors       = ["Ben Marini"]
+  gem.email         = ["bmarini@gmail.com"]
+  gem.description   = %q{TODO: Write a gem description}
+  gem.summary       = %q{TODO: Write a gem summary}
+  gem.homepage      = ""
+
+  gem.files         = `git ls-files`.split($/)
+  gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
+  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
+  gem.require_paths = ["lib"]
+  gem.add_runtime_dependency "rest-client"
+  gem.add_runtime_dependency "json"
+end
---
+++ lib/circonus-api.rb
@@ -0,0 +1,71 @@
+require "circonus-api/version"
+require "rest-client"
+require "json"
+
+module Circonus
+  class Api
+    def initialize(app_name=nil, api_token=nil)
+      app_name  ||= ENV['CIRCONUS_APP_NAME']
+      api_token ||= ENV['CIRCONUS_API_TOKEN']
+
+      @resource = RestClient::Resource.new('https://api.circonus.com',
+        :headers => {
+          :x_circonus_app_name => app_name,
+          :x_circonus_auth_token => api_token,
+          :accept => 'application/json'
+        }
+      )
+    end
+
+    def self.resource( plural_name, singular_name = plural_name.gsub(/s$/, '') )
+      define_method("list_#{plural_name}")     {            get("/#{singular_name}")              }
+      define_method("show_#{singular_name}")   { |id|       get("/#{singular_name}/#{id}")        }
+      define_method("create_#{singular_name}") { |data|     post("/#{singular_name}", data)       }
+      define_method("update_#{singular_name}") { |id, data| put("/#{singular_name}/#{id}", data)  }
+      define_method("delete_#{singular_name}") { |id|       delete("/#{singular_name}/#{id}")     }
+    end
+
+    resource "accounts"
+    resource "annotations"
+    resource "brokers"
+    resource "check_bundles"
+    resource "contact_groups"
+    resource "graphs"
+    resource "rule_sets"
+    resource "templates"
+    resource "users"
+    resource "worksheets"
+
+    def get(path, params={})
+      request(:get, path, params)
+    end
+
+    def put(path, params={})
+      request(:put, path, params.to_json)
+    end
+
+    def post(path, params={})
+      request(:post, path, params.to_json)
+    end
+
+    def delete(path, params={})
+      request(:delete, path, params)
+    end
+
+    def request(method, path, params)
+      begin
+        response = @resource[path].send(method, params)
+        result = deserialize(response)
+      rescue RestClient::Exception => e
+        result = deserialize(e.http_body)
+      end
+
+      return result
+    end
+
+    def deserialize(json_string)
+      JSON.parse(json_string)
+    end
+
+  end
+end
